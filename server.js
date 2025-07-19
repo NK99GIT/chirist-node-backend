e62a8b4972495179a4fffd1c2e7a1eab4cce1343
Kathir1999 <113663134+Kathir1999@users.noreply.github.com>
2025-07-19 21:18:23 +0530

    Updated server.js with latest changes

--- server.js
+++ server.js
@@ -18,4 +18,3 @@ app.use( "/api/users" ,require("./routes/user.routes"));
 // Start server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT,'0.0.0.0',() => console.log(`Server running on port ${PORT}`));
-z
